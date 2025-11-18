e532c1a7101a6cee33f0f86d4531403724846971
Adrian Orłów <adrian@orlow.me>
2025-11-18 04:13:09 +0100

    feat: add structure and router config

---
+++ src/components/another-domain/Counter/Counter.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Button from "../../shared/Button/Button";
+
+const Counter = () => {
+	const [count, setCount] = useState(0);
+
+	return (
+		<Button onClick={() => setCount((count) => count + 1)}>
+			count is {count}
+		</Button>
+	);
+};
+
+export default Counter;

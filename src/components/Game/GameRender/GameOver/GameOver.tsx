71bf2ed16a889853d2afd261f179c78fd5eb5cac
Adrian Orłów <adrian@orlow.me>
2025-12-07 03:27:07 +0100

    feat: new sounds

--- src/components/Game/GameRender/GameOver/GameOver.tsx
+++ src/components/Game/GameRender/GameOver/GameOver.tsx
@@ -1,7 +1,7 @@
-import { useEffect } from "react";
 import gameOverSound from "@/assets/sounds/game-over.wav";
-import successSound from "@/assets/sounds/success.wav";
+import successSound from "@/assets/sounds/success.mp3";
 import { LEVELS } from "@/constants/common";
+import { useEffect } from "react";
 import type { GameOver } from "../game/game";
 
 interface Props {

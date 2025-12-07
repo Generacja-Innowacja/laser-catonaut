cb185a392e2b662e0063c50c4b196fde04a9dd70
Adrian Orłów <adrian@orlow.me>
2025-12-07 01:46:39 +0100

    fix: stylesheets not being injected

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { reactRouter } from '@react-router/dev/vite';
-import tailwindcss from '@tailwindcss/vite';
-import path from 'node:path';
-import { defineConfig } from 'vite';
-import cssInjectedByJsPlugin from 'vite-plugin-css-injected-by-js';
+import path from "node:path";
+import { reactRouter } from "@react-router/dev/vite";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import cssInjectedByJsPlugin from "vite-plugin-css-injected-by-js";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [reactRouter(), tailwindcss(), cssInjectedByJsPlugin()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
